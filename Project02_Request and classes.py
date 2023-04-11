b74c27adede7a18b93948ac84bb90b1662a0691d
romanberkovich <122634642+romanberkovich@users.noreply.github.com>
2023-04-11 12:44:48 +0300

    Request and classes_Questionד number 1 and 2

---
+++ Project02_Request and classes.py
@@ -0,0 +1,30 @@
+import requests
+
+
+class My_User:
+    def __init__(self, id, email, username, name):
+        self.id = id
+        self.email = email
+        self.username = username
+        self.name = name
+
+    def __str__(self):
+        return f"id: {self.id}, email: {self.email}, username: {self.username}, name: {self.name}"
+
+
+def find_user(name):
+    url = "https://jsonplaceholder.typicode.com/users"
+    res = requests.get(url)
+    users = res.json()
+
+    for user in users:
+        if user['name'] == name:
+            my_user = My_User(user['id'], user['email'], user['username'], user['name'])
+            return my_user
+
+    return "User not found."
+
+
+name = input("Enter user name: ")
+user = find_user(name)
+print(user)
